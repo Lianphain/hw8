91b3b697bdfb7b78c5d5973ee8de1edc4e20e632
Kahlin Baughman <baughmankahlin@gmail.com>
2016-03-30 21:00:51 -1000

    Add more style to BSTNode and BST

--- src/BST.java
+++ src/BST.java
@@ -1,48 +1,210 @@
+/***************************************************************************************
+*
+* NAME: Kahlin Baughman
+*
+* HOMEWORK: 8
+*
+* CLASS: ICS 211
+*
+* INSTRUCTOR: Scott Robertson
+*
+* DATE: April 8, 2016
+*
+* FILE: BST.java
+*
+* DESCRIPTION: This file contains the methods and variables necessary to create and modify a BST
+*
+***************************************************************************************/
+
 public class BST{
+
+  /********************************************************************
+	*
+	* Method: setNext
+	*
+	* Description: Set the next variable of the current node to the desired node
+	*
+	* @param BSTNode<E> node
+	*
+	* @return None
+	*
+	********************************************************************/
 
   public BST(){
 
   }
 
+  /********************************************************************
+	*
+	* Method: setNext
+	*
+	* Description: Set the next variable of the current node to the desired node
+	*
+	* @param BSTNode<E> node
+	*
+	* @return None
+	*
+	********************************************************************/
+
   public void add(E element){
 
   }
+
+  /********************************************************************
+	*
+	* Method: setNext
+	*
+	* Description: Set the next variable of the current node to the desired node
+	*
+	* @param BSTNode<E> node
+	*
+	* @return None
+	*
+	********************************************************************/
 
   public E delete(E element){
 
   }
 
+  /********************************************************************
+	*
+	* Method: setNext
+	*
+	* Description: Set the next variable of the current node to the desired node
+	*
+	* @param BSTNode<E> node
+	*
+	* @return None
+	*
+	********************************************************************/
+
   public E search(E element){
 
   }
+
+  /********************************************************************
+	*
+	* Method: setNext
+	*
+	* Description: Set the next variable of the current node to the desired node
+	*
+	* @param BSTNode<E> node
+	*
+	* @return None
+	*
+	********************************************************************/
 
   public E mini(){
 
   }
 
+  /********************************************************************
+	*
+	* Method: setNext
+	*
+	* Description: Set the next variable of the current node to the desired node
+	*
+	* @param BSTNode<E> node
+	*
+	* @return None
+	*
+	********************************************************************/
+
   public E max(){
 
   }
+
+  /********************************************************************
+	*
+	* Method: setNext
+	*
+	* Description: Set the next variable of the current node to the desired node
+	*
+	* @param BSTNode<E> node
+	*
+	* @return None
+	*
+	********************************************************************/
 
   public E predecessor(E element){
 
   }
 
+  /********************************************************************
+	*
+	* Method: setNext
+	*
+	* Description: Set the next variable of the current node to the desired node
+	*
+	* @param BSTNode<E> node
+	*
+	* @return None
+	*
+	********************************************************************/
+
   public E successor(E element){
 
   }
+
+  /********************************************************************
+	*
+	* Method: setNext
+	*
+	* Description: Set the next variable of the current node to the desired node
+	*
+	* @param BSTNode<E> node
+	*
+	* @return None
+	*
+	********************************************************************/
 
   public int size(){
 
   }
 
+  /********************************************************************
+	*
+	* Method: setNext
+	*
+	* Description: Set the next variable of the current node to the desired node
+	*
+	* @param BSTNode<E> node
+	*
+	* @return None
+	*
+	********************************************************************/
+
   public String preorder(){
 
   }
 
+  /********************************************************************
+	*
+	* Method: setNext
+	*
+	* Description: Set the next variable of the current node to the desired node
+	*
+	* @param BSTNode<E> node
+	*
+	* @return None
+	*
+	********************************************************************/
+
   public String postorder(){
 
   }
+
+  /********************************************************************
+	*
+	* Method: setNext
+	*
+	* Description: Set the next variable of the current node to the desired node
+	*
+	* @param BSTNode<E> node
+	*
+	* @return None
+	*
+	********************************************************************/
 
   public String inorder(){
 
